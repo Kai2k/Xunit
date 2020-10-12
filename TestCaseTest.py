85ceca0143e7812a3fd4ab87c44ed104c28e13dd
Kai Armer <kai.armer@mkodo.com>
2020-10-12 22:06:38 +0100

    End of chapter 21

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -1,12 +1,24 @@
 from TestCase import TestCase
 from WasRun import WasRun
 
-class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
 
+class TestCaseTest(TestCase):
     def testTemplateMethod(self):
-        self.test.run()
-        assert("setUp testMethod tearDown " == self.test.log)
+        test = WasRun("testMethod")
+        test.run()
+        assert ("setUp testMethod tearDown " == test.log)
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert ("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert ("1 run, 1 failed" == result.summary())
+
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
--- WasRun.py
+++ WasRun.py
@@ -4,12 +4,16 @@ from TestCase import TestCase
 class WasRun(TestCase):
     def __init__(self, name):
         super().__init__(name)
+        self.log = ""
 
     def testMethod(self):
-        self.log= self.log + "testMethod "
+        self.log = self.log + "testMethod "
+
+    def testBrokenMethod(self):
+        raise Exception
 
     def setUp(self):
-        self.log= "setUp "
+        self.log = "setUp "
 
     def tearDown(self):
-        self.log= self.log + "tearDown "
+        self.log = self.log + "tearDown "
